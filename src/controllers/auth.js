25ccabd40480e6c537dd29cdc10fba0ed86d02b7
iamrajatsharma <sharmarajat687@gmail.com>
2025-02-06 15:34:34 +0530

    auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,27 +1,52 @@
-const { hashSync } = require("bcrypt")
+const { hashSync, compareSync } = require("bcrypt")
 const User = require("../models/User")
+const jwt = require("jsonwebtoken")
+
 
 const signIn = async (req, res) => {
-    console.log(req.body)
-    const { email, password } = req.body
-    return res.json({ email, password })
+    try {
+        const { email, password } = req.body;
+        const checkUser = await User.findOne({ email: email })
+        if (!checkUser) {
+            return res.json({ message: "Email is not registered with us." })
+        }
+
+        const checkPassword = compareSync(password, checkUser.password)
+        if (!checkPassword) {
+            return res.json({ message: "Incorrect Email OR Password" })
+        }
+
+        const token = jwt.sign({ userId: checkUser._id }, process.env.JWT_SECRET)
+
+        return res.json({ message: "Login Successfully", token })
+    }
+    catch (e) {
+        console.log('error')
+        return res.json({ e })
+    }
 }
 
 
 const signUp = async (req, res) => {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    const checkUser = User.findOne({ email })
-    if (!checkUser) {
-        return res.json({ message: "Email is not registered with us." })
+        const checkUser = await User.findOne({ email })
+        if (checkUser) {
+            return res.json({ message: "Email is alread registered with us." })
+        }
+        console.log(checkUser)
+        const user = new User()
+        user.name = name;
+        user.email = email;
+        user.password = hashSync(password, 10);
+        user.save();
+
+        return res.json({ message: "Account created Successfully", user: user })
     }
-    const user = new User()
-    user.name = name;
-    user.email = email;
-    user.password = hashSync(password, 10);
-    user.save();
-
-    return res.json({ message: "Account created Successfully", user: user })
+    catch (e) {
+        return res.json(e)
+    }
 }
 
 
--- utils/database.js
+++ utils/database.js
@@ -2,11 +2,4 @@ const mongoose = require("mongoose")
 
 const conn = mongoose.connect(process.env.DATABASE_URL)
 
-// mongoose.connection('on', (msg) => {
-//     console.log('DB Connected')
-// })
-// mongoose.error('', (msg) => {
-//     console.log('DB Error')
-// })
-
 module.exports = conn
